83e2767813db30ffd246907363b8b7c4553c78ac
Alex Batts <alex.batts05@gmail.com>
2022-08-24 22:44:01 -0400

    Fixed link preview title and bumped up version

--- clean-fairfax-blocks.php
+++ clean-fairfax-blocks.php
@@ -5,7 +5,7 @@
  * Description:       A collection of blocks specifically designed for the Clean Fairfax site.
  * Requires at least: 5.9
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           1.0.1
  * Author:            Alex Batts
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
--- templates/link-preview-card.php
+++ templates/link-preview-card.php
@@ -13,7 +13,7 @@
     <div class="bottom-0 px-8 text-right">
         <a href="<?php echo $attributes['link_url'] ?>" class="no-underline" target="<?php $attributes['opens_in_new_tab'] == true ? '' : '_blank' ?>">
             <button type=" button" class="inline-flex items-center px-4 py-2 mt-2 mb-8 space-x-4 font-medium text-white transition-all border border-transparent rounded-md shadow-sm bg-primary hover:brightness-105 focus:outline-none">
-                <?php echo ($attributes['link_text'] != null ? $attributes['link_text'] : 'Read more') ?>
+                <?php echo ($attributes['link_title'] != null ? $attributes['link_title'] : 'Read more') ?>
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
                 </svg>
